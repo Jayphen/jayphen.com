3660499ca8e0bd6a63f1638a223d5a70db3debe2
Jayphen <software@jayphen.com>
2021-06-26 17:19:24 +0200

    Add layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,42 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/blog">Words</a>
+  <span>That's it. That's the whole nav.</span>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<style global>
+  nav,
+  main {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+      'Open Sans', 'Helvetica Neue', sans-serif;
+    line-height: 1.5;
+    max-width: 65ch;
+  }
+
+  nav {
+    border-bottom: 1px solid silver;
+    display: flex;
+  }
+  nav > * {
+    font-size: 0.875rem;
+    padding: 1em;
+  }
+  nav span {
+    margin-left: auto;
+  }
+
+  h1 span {
+    border-bottom: 1px dotted #c0c0c0;
+  }
+
+  a {
+    color: #3f51b5;
+  }
+  a:hover {
+    color: #9c27b0;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,68 +1,31 @@
-<nav>
-  <a href="/">Home</a>
-  <a href="/blog">Bloggie</a>
-  <span>That's it. That's the whole nav.</span>
-</nav>
-<main>
-  <h1>
-    Hi, I'm <span
-      title="Little-known fact: Jayphen is actually my nickname; my real name is Stephen"
-      >Jayphen</span
-    >
-  </h1>
-  <p>
-    I'm a senior web engineer at <a href="https://n26.com" target="_blank">N26</a>. I was born in
-    Sydney, Australia in the 80's and settled in Vienna, Austria in the 10's
-    <small>(are we calling it that?).</small>
-  </p>
-  <p>
-    At N26 I am working on the web engineering team in Vienna. I'm responsible for the web banking
-    application, and at the moment my team is working on the new <a
-      href="https://techcrunch.com/2021/04/15/challenger-bank-n26-to-offer-insurance-products/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJC9hYTRpYTBkYuX3N_E_85uSQBz10WV7M1rzWclIpCf8w6aEHD8Jjg0tq87uPuWCFhLB6yDP3JlYIjD5Z8hox-M63509APysuGKzYel0CEwjGhZtggAuDSg-m292ORJIILjH8hUhtijfSqcC_y-QkV08qJxnBJiLXPiVZOthgPs"
-      >Insurance offerings</a
-    >.
-  </p>
-  <p>I don't have a lot to say right now, but maybe one day there will be a blog here.</p>
-  <p>For now, I'm probably more active elsewhere:</p>
-  <ul>
-    <li><a href="https://twitter.com/Jayphen">Twitter @jayphen</a></li>
-    <li><a href="https://twitter.com/Jayphen">Instagram @jayphen</a></li>
-    <li><a href="https://github.com/Jayphen">Github @jayphen</a></li>
-  </ul>
+<h1>
+  Hi, I'm <span title="Little-known fact: Jayphen is actually my nickname; my real name is Stephen"
+    >Jayphen</span
+  >
+</h1>
+<p>
+  I'm a senior web engineer at <a href="https://n26.com" target="_blank">N26</a>. I was born in
+  Sydney, Australia in the 80's and settled in Vienna, Austria in the 10's
+  <small>(are we calling it that?).</small>
+</p>
+<p>
+  At N26 I am working on the web engineering team in Vienna. I'm responsible for the web banking
+  application, and at the moment my team is working on the new <a
+    href="https://techcrunch.com/2021/04/15/challenger-bank-n26-to-offer-insurance-products/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAJC9hYTRpYTBkYuX3N_E_85uSQBz10WV7M1rzWclIpCf8w6aEHD8Jjg0tq87uPuWCFhLB6yDP3JlYIjD5Z8hox-M63509APysuGKzYel0CEwjGhZtggAuDSg-m292ORJIILjH8hUhtijfSqcC_y-QkV08qJxnBJiLXPiVZOthgPs"
+    >Insurance offerings</a
+  >.
+</p>
+<p>I don't have a lot to say right now, but maybe one day there will be a blog here.</p>
+<p>For now, I'm probably more active elsewhere:</p>
+<ul>
+  <li><a href="https://twitter.com/Jayphen">Twitter @jayphen</a></li>
+  <li><a href="https://twitter.com/Jayphen">Instagram @jayphen</a></li>
+  <li><a href="https://github.com/Jayphen">Github @jayphen</a></li>
+</ul>
 
-  <p>p.s. <a href="https://plants.jayphen.dev/plants" target="_blank">look at my plants</a></p>
-  <p>
-    p.p.s. this silly little page was built with <a href="https://kit.svelte.dev/">SvelteKit</a>. Go
-    and check it out! It's the successor to <a href="https://sapper.svelte.dev/">Sapper</a>, an OSS
-    project that I contributed to a little bit.
-  </p>
-</main>
-
-<style>
-  nav,
-  main {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-      'Open Sans', 'Helvetica Neue', sans-serif;
-    line-height: 1.5;
-    max-width: 65ch;
-  }
-
-  nav {
-    border-bottom: 1px solid silver;
-    padding: 1em 0;
-  }
-  nav > * {
-    font-size: 0.875rem;
-  }
-
-  h1 span {
-    border-bottom: 1px dotted #c0c0c0;
-  }
-
-  a {
-    color: #3f51b5;
-  }
-  a:hover {
-    color: #9c27b0;
-  }
-</style>
+<p>p.s. <a href="https://plants.jayphen.dev/plants" target="_blank">look at my plants</a></p>
+<p>
+  p.p.s. this silly little page was built with <a href="https://kit.svelte.dev/">SvelteKit</a>. Go
+  and check it out! It's the successor to <a href="https://sapper.svelte.dev/">Sapper</a>, an OSS
+  project that I contributed to a little bit.
+</p>
